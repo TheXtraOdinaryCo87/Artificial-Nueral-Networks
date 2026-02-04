65f331e5d61b76fa7360e9668d4e6691cdf9320a
unknown <thextraodinaryco87@gmail.com>
2026-02-04 21:03:03 +0530

    new testing

--- newTest.py
+++ newTest.py
@@ -1,3 +1,12 @@
+import numpy as np
+
 X = [[1 ,2, 3, 2.5],
     [2, 5, -1, 2],
     [-1.5, 2.7, 3.3, -0.8]]
+
+
+class Layer_Dense:
+    def __init__(self):
+        pass
+    def forward(self):
+        pass
